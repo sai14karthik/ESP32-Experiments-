b16027cdd5cf9824a837fee3ce7ce4b624d2f209
Sai Karthik <sai14karthik@gmail.com>
2026-08-26 09:58:14 -0400

    update firmware

--- firmware/CameraWebServerWiFi/CameraWebServerWiFi.ino
+++ firmware/CameraWebServerWiFi/CameraWebServerWiFi.ino
@@ -10,8 +10,12 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "SpectrumSetup-EB9C";
-const char *password = "unitedvideo788";
+const char *ssid = "LabHealthSecurePSK";
+const char *password = "ZLMKAQm@UV2e9g8r7GW!";
+// const char *ssid = "SpectrumSetup-EB9C";
+// const char *password = "unitedvideo788";
+// const char *ssid = "SaiPhone";
+// const char *password = "123456789";
 
 void startCameraServer();
 void setupLedFlash();
